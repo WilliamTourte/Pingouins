46d70e90baa90d9440f8aaa8a33b33e386f9c575
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-05-22 22:14:16 +0200

    Les pingouins se font supprimer

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from dal.dalImpl import get_all_pingouins, get_pingouin, delete_pingouin, update_pingouin
 
 app = Flask(__name__)
 
+@app.route('/delete_pingouin/<int:id>', methods=['DELETE'])
+def delete_pingouin_route(id):
+    delete_pingouin(id)
+    return jsonify({'status': 'success'})
 
 @app.route('/')
 def index():
     print("Index Page")
     pingouins = get_all_pingouins()
-    print(pingouins)
+
     return render_template('accueil.html', pingouins=pingouins)
 
 
