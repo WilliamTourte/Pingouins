a8b8831794b9f76efea950360ace78c0b22a484a
WilliamTourte <damien.hugonnard2025@campus-eni.fr>
2025-05-22 22:36:24 +0200

    Les pingouins se font modifier mais pas valider

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ def delete_pingouin_route(id):
     delete_pingouin(id)
     return jsonify({'status': 'success'})
 
+
+@app.route('/update_pingouin/<int:id>', methods=['GET', 'POST'])
+def update_pingouin_route(id):
+    if request.method == 'POST':
+        # Logique pour mettre à jour le pingouin dans la base de données
+        update_pingouin(id)
+        return jsonify({"status": "success"})
+    else:
+        # Récupérer les informations du pingouin
+        pingouin = get_pingouin(id)
+        if pingouin:
+            return render_template('modification.html', pingouin=pingouin)
+        else:
+            return "Pingouin non trouvé", 404
 @app.route('/')
 def index():
     print("Index Page")
